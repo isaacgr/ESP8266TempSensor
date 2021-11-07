0da3a6518c88a12310c4cc8ea2677a22c545a64f
isaacgr <isaacgrowell@gmail.com>
2021-11-06 23:55:18 -0400

    Update with new syntax and libraries. Improve display output.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
-#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 #include <WiFiClient.h>
-#include <Adafruit_Sensor.h>
+#include <WiFiClientSecureBearSSL.h>
+// #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 #include "SSD1306.h"
-
-#define DHTPIN 2
+#include <base64.h>
+#include "env.h" // environement variables
 
 #define DHTTYPE DHT22
 #define DHTPIN 13
@@ -18,60 +19,69 @@ DHT dht(DHTPIN, DHTTYPE);
 WiFiClient client;
 bool POST = false;
 int counter = 0;
+int id = 1;
 
-void displayTempHumid(float temp, float humidity, float temp_f)
+int displayTempHumid(float temp, float humidity, float temp_f)
 {
   if (isnan(humidity) || isnan(temp) || isnan(temp_f))
   {
     display.clear(); // clearing the display
     display.drawString(5, 0, "Failed DHT");
-    return;
+    return -1;
   }
   display.clear();
-  display.drawString(0, 0, "Humidity: " + String(humidity) + "%\t");
-  display.drawString(0, 20, "Temp: " + String(temp) + "C");
-  display.drawString(0, 40, "             " + String(temp_f) + "F");
+  display.drawString(0, 0, String(humidity) + "%");
+  display.drawString(0, 20, String(temp) + "C / " + String(temp_f) + "F");
+  return 0;
 }
 
-void postTemperature()
+int postTemperature(float temp, float humidity, float temp_f)
 {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  float f = dht.readTemperature(true);
+  StaticJsonDocument<192> doc;
 
-  const size_t bufferSize = JSON_OBJECT_SIZE(5);
-  DynamicJsonBuffer jsonBuffer(bufferSize);
+  doc["jsonrpc"] = "2.0";
+  doc["method"] = "set.temperature";
+  doc["id"] = id;
+  id++;
 
-  JsonObject &root = jsonBuffer.createObject();
-  root["temp"] = t;
-  root["humid"] = h;
-  root["temp_f"] = f;
-  root["loc"] = "living room";
-  root["key"] = "secretsauce";
+  JsonObject params = doc.createNestedObject("params");
+  params["temperature_C"] = temp;
+  params["humidity"] = humidity;
+  params["temperature_F"] = temp_f;
+  params["location"] = "bedroom";
 
   char JSONmessageBuffer[300];
-  root.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+  serializeJson(doc, JSONmessageBuffer);
   // Serial.println(JSONmessageBuffer);
 
+  String credentials = CREDENTIALS;
+  String encodedCredentials = base64::encode(credentials);
+
+  // Serial.println(encodedCredentials);
+
+  std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+  client->setInsecure();
   HTTPClient http; //Declare object of class HTTPClient
 
-  http.begin("http://irowell-temperature.herokuapp.com/api/temp"); //Specify request destination
-  http.addHeader("Content-Type", "application/json");              //Specify content-type header
-  http.end();                                                      //Close connection
-  int httpCode = http.POST(JSONmessageBuffer);                     //Send the request
-  String payload = http.getString();                               //Get the response payload
+  http.begin(*client, URL);                           //Specify request destination
+  http.addHeader("Content-Type", "application/json"); //Specify content-type header
+  http.addHeader("Authorization", "Basic " + encodedCredentials);
+  int httpCode = http.POST(JSONmessageBuffer); //Send the request
+  String payload = http.getString();           //Get the response payload
+  http.end();                                  //Close connection
 
   // Serial.println("Code:");
-  // Serial.println(httpCode);
+
   // Serial.println("Response:");
-  // Serial.println(payload); //Print request response payload
+  Serial.println(payload); //Print request response payload
+  return httpCode;
 }
 
-void ICACHE_RAM_ATTR timer1_ISR(void)
+void IRAM_ATTR timer1_ISR(void)
 {
   // Do some work
   counter++;
-  if (counter == 60)
+  if (counter == 600) // update every 10 mins
   {
     POST = true;
     counter = 0;
@@ -80,26 +90,27 @@ void ICACHE_RAM_ATTR timer1_ISR(void)
 
 void setup()
 {
-  // Serial.begin(115200);
-  WiFi.begin("VIRGIN559", "412C7934"); //WiFi connection
+  Serial.begin(115200);
+  WiFi.begin(SSID, WIFI_PASS); //WiFi connection
   while (WiFi.status() != WL_CONNECTED)
   {
+    Serial.println("Waiting for connection");
     delay(500);
-    // Serial.println("Waiting for connection");
   }
+  Serial.println("Connected.");
   dht.begin(); // initialize dht
   display.init();
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_16);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  postTemperature();
+  POST = true;
 
   // Setup interrupts
   noInterrupts();
   timer1_attachInterrupt(timer1_ISR);
   timer1_isr_init();
   timer1_enable(TIM_DIV256, TIM_EDGE, TIM_LOOP);
-  timer1_write(6250000); // 80MHz == 1sec
+  timer1_write(312500); // 80MHz == 1sec
   interrupts();
 
   //Set up ESP watchdog
@@ -109,28 +120,25 @@ void setup()
 
 void loop()
 {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  int dhGood = displayTempHumid(t, h, f);
 
   if (WiFi.status() == WL_CONNECTED)
   {
 
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-    float f = dht.readTemperature(true);
-
-    displayTempHumid(t, h, f);
-    display.display();
-    if (POST)
+    if (POST && dhGood == 0)
     {
-      // Serial.println("POST");
-      postTemperature();
-      // Set-up the next interrupt cycle
-      timer1_write(6250000); //80Mhz -> 80*10^6 = 1 second
+      int httpCode = postTemperature(t, h, f);
+      display.drawString(0, 40, "res: " + String(httpCode) + "    wifi: ok");
+      display.display();
     }
   }
   else
   {
-    display.clear();
-    display.drawString(0, 0, "Error in wifi connection");
+    display.drawString(0, 40, "wifi: no");
     display.display();
   }
   POST = false;
